4d2dfb887042b59a63190adbc7b179cc6b25def1
Mateusz Kocz <mateusz@kocz.pl>
2016-11-14 18:15:36 +0100

    feat(lib): add hex colors utilities

---
+++ src/index.spec.ts
@@ -0,0 +1,91 @@
+import {ColorUtilities} from './index'
+
+const basicHexColor = '#FFA500'
+
+const validHexColors = [
+  basicHexColor,
+  '#aaa',
+  '#123',
+  '#AAAAAA'
+]
+
+const invalidHexColors = [
+  'invalid',
+  '10AA40', // No #
+
+  // Too many or too few characters
+  '#4',
+  '#A3',
+  '#AB23',
+  '#FFAA',
+  '#AB233',
+  '#aaaaaaa',
+
+  '#GFAA500' // No 'G' on the range
+]
+
+describe('Color utilities', () => {
+  let colorUtil: ColorUtilities
+
+  beforeEach(() => {
+    colorUtil = new ColorUtilities()
+  })
+
+  describe('Dev information', () => {
+    it('should expose some information to consumers', () => {
+      expect(ColorUtilities.VERSION).toEqual(jasmine.stringMatching(/^(\d+\.){2}\d+$/))
+    })
+  })
+  
+  describe('Colors parsing', () => {
+    it('should transform hex colors into RGB values array', () => {
+      expect(colorUtil.parseHexColor('#FFF')).toEqual([255, 255, 255])
+      expect(colorUtil.parseHexColor('#FFFFFF')).toEqual([255, 255, 255])
+      expect(colorUtil.parseHexColor('#000000')).toEqual([0, 0, 0])
+      expect(colorUtil.parseHexColor('#ffa500')).toEqual([255, 165, 0])
+      expect(colorUtil.parseHexColor(basicHexColor)).toEqual([255, 165, 0])
+    })
+
+    it('should allow using shorthand hex colors', () => {
+      expect(colorUtil.parseHexColor('#FFF')).toEqual(colorUtil.parseHexColor('#FFFFFF'))
+    })
+
+    it('should treat capitalized and lower case HEX colors in the same way', () => {
+      expect(colorUtil.parseHexColor('#ffffff')).toEqual(colorUtil.parseHexColor('#FFFFFF'))
+    })
+
+    it('should inform about failed parsing', () => {
+      invalidHexColors.forEach(invalidColor => expect(() => colorUtil.parseHexColor(invalidColor)).toThrowError(TypeError))
+    })
+  })
+
+  describe('Color validation', () => {
+    it('should validate hex colors', () => {
+      validHexColors.forEach(color => expect(colorUtil.isValidHexColor(color)).toBeTruthy())
+      invalidHexColors.forEach(color => expect(colorUtil.isValidHexColor(color)).toBeFalsy())
+      expect(colorUtil.isValidHexColor(null)).toBeFalsy()
+      expect(colorUtil.isValidHexColor(undefined)).toBeFalsy()
+      expect(colorUtil.isValidHexColor('')).toBeFalsy()
+    })
+  })
+
+  describe('Colors analyze', () => {
+    it('should allow splitting the hex color inti its parts', () => {
+      expect(colorUtil.splitHexColor(basicHexColor)).toEqual(['FF', 'A5', '00'])
+      expect(colorUtil.splitHexColor('#ffa500')).toEqual(['FF', 'A5', '00']) // note the uppercases characters
+    })
+  })
+
+  describe('Colors normalization', () => {
+    it('should normalize the shorthand hex colors', () => {
+      expect(colorUtil.normalizeHexColor('#aaa')).toBe('#AAAAAA')
+      expect(colorUtil.normalizeHexColor('#aaaaaa')).toBe('#AAAAAA')
+      expect(colorUtil.normalizeHexColor('#fa5')).toBe('#FFAA55')
+      expect(colorUtil.normalizeHexColor(basicHexColor)).toBe(basicHexColor)
+    })
+
+    it('should not handle invalid hex colors', () => {
+      invalidHexColors.forEach(invalidColor => expect(() => colorUtil.normalizeHexColor(invalidColor)).toThrowError(TypeError))
+    })
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+// TODO: Should be exported?
+export type RGBColor = string
+export type hexColor = string
+export type hexValue = string
+export type hexColorValues = [hexValue, hexValue, hexValue]
+export type anyColor = RGBColor | hexColor
+export type colorValues = [number, number, number]
+
+export class ColorUtilities {
+  // TODO: Make the version number dynamic
+  static readonly VERSION = '0.0.0'
+
+  /* Parsers */
+
+  /**
+   * Parses valid hex colors into their RGB representation in base 10.
+   *
+   * @throws TypeError
+   * @param hexColor to be transformed
+   * @returns {colorValues} RGB triplet of the provided hex color
+   */
+  parseHexColor(hexColor: hexColor): colorValues {
+    return <colorValues> this.splitHexColor(this.normalizeHexColor(hexColor)).map(this.changeHexToNumber)
+  }
+
+  /* Validators */
+
+  /**
+   * Validates the provided hex color.
+   *
+   * @example
+   * "#FFA500" => true
+   * "#FFF" => true
+   * "#GGG" => false
+   * "" => false
+   *
+   * @param potentialHexColor
+   * @returns {hexColor|boolean}
+   */
+  isValidHexColor(potentialHexColor?: hexColor): boolean {
+    return potentialHexColor && /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(potentialHexColor)
+  }
+
+  /* Analyzers */
+
+  /**
+   * Splits the solid hex color string into its value parts.
+   *
+   * @example
+   * "#FFA500" => ["FF", "A5", "00"]
+   *
+   * @param hexColor to te split
+   * @returns {hexColorValues}
+   */
+  splitHexColor(hexColor: hexColor): hexColorValues {
+    return <hexColorValues> [[1, 3], [3, 5], [5, 7]]
+      .map(splitRange => hexColor.substring.apply(hexColor, splitRange).toUpperCase())
+  }
+
+  /* Normalizers */
+
+  /**
+   * Changes the shorthand hex colors into their full, 6 characters long representation. Returned value is uppercased.
+   *
+   * @example
+   * "#fff" => "#FFFFFF"
+   *
+   * @throws TypeError
+   * @param hexColor to be normalized
+   * @returns {any}
+   */
+  normalizeHexColor(hexColor: hexColor): hexColor {
+    if (!this.isValidHexColor(hexColor)) {
+      this.throwInvalidHexColor(hexColor)
+    }
+
+    if (hexColor.length === 4) {
+      return hexColor.split('')
+        .reduce((acc, digit, index) => index !== 0 ? [...acc, digit, digit] : acc, ['#'])
+        .join('')
+        .toUpperCase()
+    }
+
+    return hexColor.toUpperCase()
+  }
+
+  /* Supporting internal methods */
+
+  private changeHexToNumber(hex: string): number {
+    return parseInt(hex, 16);
+  }
+
+  /** @internal */
+  private throwInvalidHexColor(hexColor?: hexColor): never {
+    throw new TypeError(`Using invalid hex color value. Used "${hexColor}" but only #RGB and #RRGGBB are allowed.`)
+  }
+}
