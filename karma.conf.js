e825d7fde628aee08c255afbfccd8c590e54f5f3
Mateusz Kocz <mateusz@kocz.pl>
2016-11-30 15:48:42 +0100

    feat(tests): add clove reporting

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,8 @@ module.exports = function (config) {
     karmaTypescriptConfig: {
       reports: {
         "html": "coverage",
-        "text-summary": ""
+        "text": "",
+        "lcovonly": "coverage"
       }
     }
   })
