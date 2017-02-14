914a88a704b2b573d13bd8c8c368d2a781866f0c
Mateusz Kocz <mateusz@kocz.pl>
2017-02-14 18:10:57 +0100

    fix(types): make the hslColor a part of the anyColor type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,10 @@
+// Colors
 export type rgbColor = string
 export type hexColor = string
 export type hslColor = string
+export type anyColor = rgbColor | hexColor | hslColor
+
+// Values
 export type hexValue = string
 export type hexColorValues = [hexValue, hexValue, hexValue]
-export type anyColor = rgbColor | hexColor
 export type colorValues = [number, number, number]
